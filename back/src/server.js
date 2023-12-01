7bda4eaf83ebaefe0f60bd5f5c2862ba74b950bd
oumaima-arfaoui <oumaimaarfaoui22@gmail.com>
2023-12-01 17:30:49 +0100

    fixed backend

--- back/src/server.js
+++ back/src/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const db = require("../src/config/dbconnection.js");
 const port = 5000;
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,6 +22,6 @@ db.configserverdb
     console.log("err", err);
   });
 
-require('../src/routes/routes.js')(app);
+require("../src/routes/routes.js")(app);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
